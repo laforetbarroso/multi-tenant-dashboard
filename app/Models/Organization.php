56726cfc1936fbcc227e6c652070e37adcf49b3f
Pedro Barroso <laforetbarroso@gmail.com>
2025-05-13 20:49:23 +0100

    feat: project initialization

---
+++ app/Models/Organization.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Organization extends Model
+{
+    public function users()
+    {
+        return $this->belongsToMany(User::class)
+            ->withPivot('role')
+            ->withTimestamps();
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
